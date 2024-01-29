54b0685e43a9e77ee82e8e6912fef7ecf9ebe5b8
Priyansh Rathi <techiepriyansh@gmail.com>
2024-01-30 01:53:55 +0530

    Pretty print test

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,24 @@ fn test_zktsim() {
     let ckt = BooleanCircuit::from_netlist("examples/cla_adder_6b.zkt").unwrap();
     let inputs = [
         true, true, true, true, true, false, // a
-        true, false, false, false, false, false, // b
+        true, false, true, false, false, false, // b
         false, // c_in
     ];
 
-    run_zktsim(BooleanCircuitInstance::from_ckt_and_inputs(&ckt, &inputs));
+    let inst = BooleanCircuitInstance::from_ckt_and_inputs(&ckt, &inputs);
+    let w = &inst.wires;
+    println!(
+        "{} {} {} {} {} {} {}",
+        u8::from(w[8]),
+        u8::from(w[7]),
+        u8::from(w[6]),
+        u8::from(w[5]),
+        u8::from(w[4]),
+        u8::from(w[3]),
+        u8::from(w[2]),
+    );
+
+    run_zktsim(inst);
 
     println!("zktsim works!");
 }
